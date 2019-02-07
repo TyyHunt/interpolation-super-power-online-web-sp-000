171be461383f734399d72ab01b911f54956d081f
Tyler Hunt <tyhunt04@gmail.com>
2019-02-07 01:34:12 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,4 +5,4 @@ end
 
 color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
-display_rainbow(color)
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
